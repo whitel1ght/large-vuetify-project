a4dff1c95ce4e21c86ca662b88b58bd5100339c6
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-09-28 15:51:51 +0300

    two type of forms added

---
+++ src/components/forms/UserWithNumberForm.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-form
+    ref="firstFormRef"
+  >
+    <v-text-field
+      v-model="form.name"
+      label="Name"
+      required
+    />
+    <v-text-field
+      v-model="form.accessToken"
+      label="Access Token"
+      required
+    />
+    <v-text-field
+      v-model="form.refreshToken"
+      label="Refresh Token"
+      required
+    />
+  </v-form>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+const props = defineProps({
+  user: Object,
+  eventBus: Object
+})
+
+const data = {
+  name: '',
+  accessToken: '',
+  refreshToken: ''
+}
+
+// refs
+const firstFormRef = ref(null)
+
+// reactive
+const form = reactive(data)
+
+// methods
+const onClose = () => {
+  firstFormRef.value.resetValidation()
+  Object.assign(form, data)
+}
+
+const create = async data => {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve('created')
+    }, 1000)
+  })
+  onClose()
+}
+
+const update = async data => {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve('updated')
+    }, 1000)
+  })
+  onClose()
+}
+
+const submit = async () => {
+  const { valid = false } = await firstFormRef.value.validate()
+  if (!valid) return
+
+  const data = {
+    name: form.name,
+    accessToken: form.accessToken,
+    refreshToken: form.refreshToken
+  }
+
+  if (props.userData) {
+    await update(userData)
+  } else {
+    await create(userData)
+  }
+}
+
+props.eventBus.on('submit', () => {
+  emit('submit:form')
+  onClose()
+})
+</script>
---
+++ src/components/forms/UserWithPasswordForm.vue
@@ -0,0 +1,9 @@
+<template>
+  User with Password
+</template>
+
+<script setup>
+const props = defineProps({
+  eventBus: Object
+})
+</script>
