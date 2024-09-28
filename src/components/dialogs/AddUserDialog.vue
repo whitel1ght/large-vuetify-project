6ddd7514d009b1fdc9cb2ee22064f01eb4ae406c
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-09-28 15:51:37 +0300

    add user dialog component logic

--- src/components/dialogs/AddUserDialog.vue
+++ src/components/dialogs/AddUserDialog.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-dialog v-model="show">
+  <v-dialog
+    v-model="show"
+    width="600"
+  >
     <v-stepper v-model="step">
       <v-stepper-header>
         <v-stepper-item
@@ -7,63 +10,112 @@
           step="1"
           :value="1"
         >
-          <!-- {{ $t('DMS.Stepper.selectDmsType') }} -->
+          Select User Type
         </v-stepper-item>
 
         <v-divider />
+
         <v-stepper-item
           :complete="step > 2"
           step="2"
           :value="2"
         >
-          <!-- {{ $t('DMS.Stepper.addDms') }} -->
+          Add User
         </v-stepper-item>
       </v-stepper-header>
 
       <v-stepper-window>
         <v-stepper-window-item :value="1">
-          <!-- <dms-type-form v-model="form.dmsType" /> -->
+          <user-type-form v-model="type" />
         </v-stepper-window-item>
 
         <v-stepper-window-item :value="2">
-          <!-- <component -->
-          <!--   :is="nextStepForm" -->
-          <!--   :event-bus="eventBus" -->
-          <!--   :update-with="props.updateWith" -->
-          <!--   :create-with="props.createWith" -->
-          <!--   @close:dialog="close" -->
-          <!--   @submit:form="submit" -->
-          <!-- /> -->
+          <component
+            :is="nextStepForm"
+            :event-bus="eventBus"
+            @close:dialog="close"
+            @submit:form="submit"
+          />
         </v-stepper-window-item>
       </v-stepper-window>
 
-      <!-- <stepper-actions -->
-      <!--   :show-back="form.step > 1" -->
-      <!--   :submit-text="form.step === 2 ? $t('Buttons.submit') : $t('Buttons.continue')" -->
-      <!--   @back="prev" -->
-      <!--   @cancel="form.step === 2 ? emitClose() : close()" -->
-      <!--   @submit="form.step === 2 ? emitSubmit() : next()" -->
-      <!-- /> -->
+      <div class="d-flex justify-space-between align-center p-4">
+        <v-btn
+          v-if="step > 1"
+          variant="text"
+          dark
+          @click="prev"
+        >
+          Back
+        </v-btn>
+
+        <div class="d-flex flex-grow-1 justify-end">
+          <v-btn
+            variant="text"
+            @click="show = false"
+            >
+            Cancel
+          </v-btn>
+
+          <v-btn
+            variant="text"
+            @click="submit"
+          >
+            Submit
+          </v-btn>
+        </div>
+      </div>
     </v-stepper>
   </v-dialog>
 </template>
 
 <script setup>
 import { ref, computed, defineAsyncComponent } from 'vue'
+// components
+import UserTypeForm from '@/components/forms/UserTypeForm.vue'
+// utils
+import mitt from 'mitt'
 
-const props = defineProps({ dialog: Boolean })
+const props = defineProps({ modelValue: Boolean })
+const emit = defineEmits(['update:modelValue'])
 
-// constants
-const types = [
-  'Admin',
-  'User'
-]
-
+// consts
+const eventBus = mitt()
 // refs
 const step = ref(1)
+const type = ref('User With Number')
+
 // computeds
 const show = computed({
-  get: () => props.dialog,
-  set: value => emit('update:dialog', value)
+  get: () => props.modelValue,
+  set: value => emit('update:modelValue', value)
 })
+
+const nextStepForm = computed(() => {
+  const forms = {
+    UserWithNumberForm: defineAsyncComponent(() => import('@/components/forms/UserWithNumberForm.vue')),
+    UserWithPasswordForm: defineAsyncComponent(() => import('@/components/forms/UserWithPasswordForm.vue'))
+  }
+
+  return forms[type.value.replace(/ /g, '') + 'Form']
+})
+
+// methods
+function prev() {
+  step.value--
+}
+
+function next() {
+  step.value++
+}
+
+function submit() {
+  if (step.value === 2) {
+    // submit form
+    eventBus.emit('submit')
+    console.log('submit form')
+  } else {
+    next()
+  }
+}
 </script>
