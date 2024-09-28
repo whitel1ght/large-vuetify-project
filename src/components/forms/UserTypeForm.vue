e151301272c2bbc1d7641cca2e8deb72080123cc
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-09-28 15:00:23 +0300

    user type form

---
+++ src/components/forms/UserTypeForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-form>
+    <v-select
+      v-model="internalModel"
+      :items="types"
+      label="User Type"
+      required
+    />
+  </v-form>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue'
+
+const props = defineProps({ modelValue: String })
+const emit = defineEmits(['update:modelValue'])
+
+const internalModel = computed({
+  get: () => props.modelValue,
+  set: val => emit('update:modelValue', val)
+})
+
+// composable with translations here ?
+const types = [ 'User With Number', 'User With Password' ]
+
+</script>
