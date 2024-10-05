84c245352143213893677897c6d60c2c7792a3a3
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-10-05 15:13:57 +0300

    formatting for User.ts

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,6 +2,14 @@ import type { UserWithNumber } from './UserWithNumber'
 import type { UserWithToken } from './UserWithToken.ts'
 import type { UserWithPassword } from './UserWithPassword.ts'
 
-export enum UserType { WITH_NUMBER, WITH_TOKEN, WITH_PASSWORD }
+export enum UserType {
+  WITH_NUMBER,
+  WITH_TOKEN,
+  WITH_PASSWORD
+}
 
-export type User = UserWithNumber | UserWithToken | UserWithPassword
+export type User = UserWithNumber
+  | UserWithToken
+  | UserWithPassword
+
+// feature:add-user
