f8761db2dcba96987604db6a495003708e3cadc1
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-10-08 16:04:41 +0300

    add user script updates User.ts file

---
+++ dev-tools/add-user.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use autodie;
+use Cwd qw(cwd);
+use File::Copy;
+
+# - read argumnet ts file
+# - import to User.ts & extend enum & extend type User (src/types/User.ts)
+# - update USER_TYPES_MAP (src/utils/users.js)
+# - update userResponse (src/mock/userResponse.ts)
+# - update json file with translations (src/lang/en/Users.json)
+# - create form (src/components/forms/)
+# - import form to AddUserDialog (src/components/dialogs/AddUserDialog.vue)
+# - import form to EditUserDialog (src/components/dialogs/EditUserDialog.vue)
+
+sub main {
+  my $ts_file = $ARGV[0];
+
+  my ($user_type, $enum_key, @keys) = parse_user_ts($ts_file);
+
+  print "User type: ${user_type}\n";
+  print "Keys: @keys\n";
+  print "Enum key: ${enum_key}\n";
+
+  update_user_ts($user_type, $enum_key);
+}
+
+# sets user type, enum key and keys
+# so we can update some files using this data
+sub parse_user_ts {
+  my ($user) = @_;
+  open my $input, '<', $user;
+
+  my @keys;
+  my $user_type;
+  my $enum_key;
+
+  while (my $line = <$input>) {
+    if ($line =~ /export interface (\S*) \{$/) {
+      $user_type = $1;
+    }
+
+    if ($line =~ /(\S*): (\S*)$/) {
+      push @keys, $1;
+    }
+  }
+
+  $enum_key = substr(uc(join('_', split(/(?=[A-Z])/, $user_type))), 5);
+
+  close $input;
+  return ($user_type, $enum_key, @keys);
+}
+
+sub update_user_ts {
+  my ($user_type, $enum_key) = @_;
+  print "User type: $user_type\n";
+
+  my $user_ts = 'src/types/User.ts';
+  my $temp_file = 'src/types/User.ts.temp';
+
+  open my $input, '<', $user_ts;
+  open my $output, '>', $temp_file;
+
+  while (my $line = <$input>) {
+    print $output $line;
+
+    if ($line =~ /import type \{ UserWithPassword./) {
+      print $output "import type { $user_type } from './$user_type'\n";
+    }
+
+    if ($line =~ /export enum UserType \{/) {
+      print $output "  ${enum_key},\n";
+    }
+
+    if ($line =~ /export type User./) {
+      print $output "  | $user_type\n";
+    }
+  }
+
+  close $input;
+  close $output;
+
+  move($temp_file, $user_ts);
+}
+
+main();
