22806f9fe966982e9ee4c7f23b6995e9e22286d7
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2024-10-15 15:38:01 +0300

    how slide

---
+++ src/pages/how.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex flex-column justify-center align-center h-100 wrapper">
+    <h1>HOW</h1>
+
+    <p class="text-center">
+    The importance of not just thinking about what you’re doing, but also how you’re doing it, lies in the realization that there is more to success than
+simply completing tasks. It involves considering the manner in which you approach and execute your actions, as this can greatly impact the quality of
+your work and the overall outcome. By paying attention to both the “what” and the “how,” you can refine your processes, identify areas for improvement,
+and ultimately achieve greater success in your endeavors.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
